8866353b9cb026b596c452efd15ab0e4a1f214a8
ttugbatural-web <ttugba.tural@gmail.com>
2026-09-01 18:59:10 +0300

    Update Supabase anon key in auth.js

--- admin/auth.js
+++ admin/auth.js
@@ -7,7 +7,7 @@ const SUPABASE_URL =
     "https://rfqiffvudbmphvklxetf.supabase.co";
 
 const SUPABASE_ANON_KEY =
-    "BURAYA_ÇALIŞAN_ANON_KEYİNİ_YAPIŞTIR";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmcWlmZnZ1ZGJtcGh2a2x4ZXRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxNDM2MDcsImV4cCI6MjEwMzcxOTYwN30.2L9Yjkc3kK1zfo4muGy18XXX4COaB5RMAxSLt-bDt_c";
 
 
 /* ==================================================
